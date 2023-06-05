0e30912eabe1823f0cbea12bb09fca6361770d82
arthev <arthur@evensen.space>
2023-06-05 23:13:36 +0200

    Add DEFRECGENERIC (untested), refactor basically everything in recorder-macros

--- src/recorder-macros.lisp
+++ src/recorder-macros.lisp
@@ -1,6 +1,33 @@
 (in-package :autoCLaracterization)
 
-;;;; DEFRECFUN section
+(defun generate-function-body (nexty-form &key test
+                                            custom-test
+                                            custom-test-supplied-p
+                                            name
+                                            required-params
+                                            optional-params
+                                            rest-param
+                                            keyword-params)
+  "NEXTY-FORM is doing the 'actual function call' etc. It's a form to ,slice in."
+  (with-gensyms (invocation-form return-values result-form)
+    `(let* ((,invocation-form
+              ,(generate-generate-invocation-form name
+                                                  required-params
+                                                  optional-params
+                                                  rest-param
+                                                  keyword-params))
+            (,return-values
+              (multiple-value-list
+               ,nexty-form))
+            (,result-form
+              (generate-result-form ,return-values)))
+       (record-characterization-test
+        ,invocation-form
+        ,result-form
+        ,@(if custom-test-supplied-p
+              `(:custom-test ',custom-test)
+              `(:test ',test)))
+       (values-list ,return-values))))
 
 (defun generate-generate-invocation-form (name required-params optional-params rest-param keyword-params)
   (assert (fleshed-out-optional-params-p optional-params))
@@ -74,14 +101,7 @@
     ,@(when aux-params
         `(&aux ,@aux-params))))
 
-(defmacro defrecfun (name-and-options lambda-list &body body)
-  "Macro to set up automatic capture of characterization tests for arbitrary functions with minimal setup/hassle.
-
-NAME-AND-OPTIONS can be the function name, or it can be a list conforming to the below destructuring-bind on it, where TEST is the comparison test to check for pairwise same functional result. (i.e. compares primary return values using TEST, secondary return values using TEST, and so forth.) If the case is complex and you want different types of comparisons for the different return values, you can instead supply CUSTOM-TEST which must be a function of two arguments, one of which will be the list of return-values as per the expected setup in the generated test, and one of which will be the return-values list of the function call in the test itself.
-
-BODY has the same semantics as in DEFUN. LAMBDA-LIST has the same semantics as in DEFUN, except that optional and keyword parameters get normalized and fleshed out to contain suppliedp vars too (using gensyms). These changes should be invisible to the end programmer.
-
-DEFRECFUN is meant to be 'dropped in' instead of DEFUN for existing functions. Since DEFRECFUN tests the function as a functional interface (i.e. takes X input, gives Y output), functions that work by side-effects might not be suitable, and some functions might be more suitable after splitting up to expose hidden inner functional interfaces etc."
+(defun flesh-out-lambda-list (lambda-list)
   (multiple-value-bind (required-params
                         optional-params
                         rest-param
@@ -90,98 +110,59 @@ DEFRECFUN is meant to be 'dropped in' instead of DEFUN for existing functions. S
                         aux-params
                         keys-p)
       (alexandria:parse-ordinary-lambda-list lambda-list)
-    (destructuring-bind (name &key
-                                (test '#'eql)
-                                (custom-test nil custom-test-supplied-p))
-        (listify name-and-options)
-      (with-gensyms (invocation-form return-values result-form inner-fn)
-        (let* ((new-body (subst inner-fn name body))
-               (optional-params (flesh-out-normalized-optional-params optional-params))
-               (keyword-params (flesh-out-normalized-keyword-params keyword-params))
-               (lambda-list (make-ordinary-lambda-list required-params
-                                                       optional-params
-                                                       rest-param
-                                                       keyword-params
-                                                       allow-other-keys-p
-                                                       aux-params
-                                                       keys-p)))
+    (let* ((optional-params (flesh-out-normalized-optional-params
+                             optional-params))
+           (keyword-params (flesh-out-normalized-keyword-params
+                            keyword-params)))
+      (make-ordinary-lambda-list required-params
+                                 optional-params
+                                 rest-param
+                                 keyword-params
+                                 allow-other-keys-p
+                                 aux-params
+                                 keys-p))))
+
+;;;; DEFRECFUN section
+
+(defmacro defrecfun (name-and-options lambda-list &body body)
+  "Macro to set up automatic capture of characterization tests for arbitrary functions with minimal setup/hassle.
+
+NAME-AND-OPTIONS can be the function name, or it can be a list conforming to the below destructuring-bind on it, where TEST is the comparison test to check for pairwise same functional result. (i.e. compares primary return values using TEST, secondary return values using TEST, and so forth.) If the case is complex and you want different types of comparisons for the different return values, you can instead supply CUSTOM-TEST which must be a function of two arguments, one of which will be the list of return-values as per the expected setup in the generated test, and one of which will be the return-values list of the function call in the test itself.
+
+BODY has the same semantics as in DEFUN. LAMBDA-LIST has the same semantics as in DEFUN, except that optional and keyword parameters get normalized and fleshed out to contain suppliedp vars too (using gensyms). These changes should be invisible to the end programmer.
+
+DEFRECFUN is meant to be 'dropped in' instead of DEFUN for existing functions. Since DEFRECFUN tests the function as a functional interface (i.e. takes X input, gives Y output), functions that work by side-effects might not be suitable, and some functions might be more suitable after splitting up to expose hidden inner functional interfaces etc."
+  (destructuring-bind (name &key
+                              (test '#'eql)
+                              (custom-test nil custom-test-supplied-p))
+      (listify name-and-options)
+    (with-gensyms (inner-fn)
+      (let* ((new-body (subst inner-fn name body))
+             (lambda-list (flesh-out-lambda-list lambda-list)))
+        (multiple-value-bind (required-params
+                              optional-params
+                              rest-param
+                              keyword-params
+                              allow-other-keys-p
+                              aux-params
+                              keys-p)
+            (alexandria:parse-ordinary-lambda-list lambda-list)
+          (declare (ignore allow-other-keys-p aux-params keys-p))
           `(defun ,name ,lambda-list
-             (let* ((,invocation-form
-                      ,(generate-generate-invocation-form name
-                                                          required-params
-                                                          optional-params
-                                                          rest-param
-                                                          keyword-params))
-                    (,return-values
-                      (multiple-value-list
-                       (labels ((,inner-fn ,lambda-list
-                                  ,@new-body))
-                         ,@new-body)))
-                    (,result-form
-                      (generate-result-form ,return-values)))
-               (record-characterization-test
-                ,invocation-form
-                ,result-form
-                ,@(if custom-test-supplied-p
-                      `(:custom-test ',custom-test)
-                      `(:test ',test)))
-               (values-list ,return-values))))))))
+             ,(generate-function-body
+               `(labels ((,inner-fn ,lambda-list
+                           ,@new-body))
+                  ,@new-body)
+               :test test
+               :custom-test custom-test
+               :custom-test-supplied-p custom-test-supplied-p
+               :name name
+               :required-params required-params
+               :optional-params optional-params
+               :rest-param rest-param
+               :keyword-params keyword-params)))))))
 
 ;;;; DEFRECGENERIC section
-
-;; Observations:
-;; We can't easily test the functional interface of a single method, since there's
-;; not really any clean way to call it. The various :arounds, :befores, :afters
-;; might be very relevant to what the output of the generic call looks like (e.g.
-;; by setting up context appropriately). Hence, we can't do DEFRECMETHOD, but I
-;; think we _can_ do DEFRECGENERIC!
-
-;; Ideas:
-;; 1) In case of &key &allow-other-keys without a &rest, we'll need to insert
-;; an automatic &rest I think. Verified quickly in the REPL that this doesn't
-;; lead to trouble, though, so's all good :)
-;; 2) Supply a weirdo method combination to ensure we can wrap stuff around
-;; the whole gamut. Obviously, if method combination stuff gets supplied by
-;; the user (in the DEFRECGENERIC!!) form, then we error and warn that it's
-;; not supported. Supplying an alternate method combination is obviously a
-;; little messy, but while not perfect it at least meets the goal of easily
-;; slotting into the common cases. (Surely 95%+, at least, of generics use
-;; the standard method combination?)
-
-;; Attack:
-;; CLHS (http://www.lispworks.com/documentation/HyperSpec/Body/m_defi_4.htm)
-;; contains, as an example, the following:
-
-;; ;The default method-combination technique
-;;  (define-method-combination standard ()
-;;          ((around (:around))
-;;           (before (:before))
-;;           (primary () :required t)
-;;           (after (:after)))
-;;    (flet ((call-methods (methods)
-;;             (mapcar #'(lambda (method)
-;;                         `(call-method ,method))
-;;                     methods)))
-;;      (let ((form (if (or before after (rest primary))
-;;                      `(multiple-value-prog1
-;;                         (progn ,@(call-methods before)
-;;                                (call-method ,(first primary)
-;;                                             ,(rest primary)))
-;;                         ,@(call-methods (reverse after)))
-;;                      `(call-method ,(first primary)))))
-;;        (if around
-;;            `(call-method ,(first around)
-;;                          (,@(rest around)
-;;                           (make-method ,form)))
-;;            form))))
-
-;; My idea is to introduce a new method-combination, SUPERSTANDARD, which has
-;; the STANDARD behaviour except for one modification: Allow the specification of
-;; SUPERAROUND which wraps around all the arounds again (and around all the
-;; before/after if there's no arounds). Only permit a single SUPERAROUND method,
-;; error out and warn if there's multiple. Users should never specify a SUPERAROUND
-;; themselves, it's simply there as part of the machinery to get DEFRECGENERIC
-;; to work.
 
 (defun multiple-superarounds-error-string-p (e)
   (search "Only 1 :superaround permitted per generic" (format nil "~A" e)))
@@ -226,3 +207,82 @@ can be added without modifying any existing DEFMETHOD forms. Balks if multiple
                             (make-method ,form))))
             (t
              form)))))
+
+(defun generate-defrecgeneric-method-lambda-list (lambda-list)
+  "Necessary since e.g. a generic might have (&key &allow-other-keys)
+without a &rest for example."
+  (multiple-value-bind (required-params
+                        optional-params
+                        rest-param
+                        keyword-params
+                        allow-other-keys-p
+                        aux-params
+                        keys-p)
+      (alexandria:parse-ordinary-lambda-list lambda-list)
+    (let ((congruent-lambda-list
+            (if (or rest-param (not keys-p))
+                lambda-list
+                (make-ordinary-lambda-list required-params
+                                           optional-params
+                                           (gensym "&REST")
+                                           keyword-params
+                                           allow-other-keys-p
+                                           aux-params
+                                           keys-p))))
+      congruent-lambda-list)))
+
+(defmacro defrecgeneric (name-and-options lambda-list &body options)
+  "Macro to set up automatic capture of characterization tests for arbitrary generic functions with minimal setup/hassle.
+
+NAME-AND-OPTIONS can be the generic function name, or it can be a list conforming to the below destructuring-bind on it, where TEST is the comparison test to check for pairwise same functional result. (i.e. compares primary return values using TEST, secondary return values using TEST, and so forth.) If the case is complex and you want different types of comparisons for the different return values, you can instead supply CUSTOM-TEST which must be a function of two arguments, one of which will be the list of return-values as per the expected setup in the generated test, and one of which will be the return-values list of the call in the test itself.
+
+OPTIONS has the same semantics as in DEFGENERIC save that :METHOD-COMBINATION is not available (because DEFRECGENERIC fills it in automatically). LAMBDA-LIST has the same semantics as in DEFGENERIC.
+
+DEFRECGENERIC is meant to be 'dropped in' instead of DEFGENERIC for existing generics/methods. Since DEFRECGENERIC tests the generic function as a functional interface (i.e. takes X input, gives Y output), generic functions that work by side-effects might not be suitable, and some might be more suitable after splitting up to expose hidden inner functional interfaces etc."
+  (let ((entry (find :method-combination options :key #'car)))
+    (when entry
+      (error "DEFRECGENERIC doesn't permit option ~S" entry)))
+  (destructuring-bind (name &key
+                              (test '#'eql)
+                              (custom-test nil custom-test-supplied-p))
+      (listify name-and-options)
+    (let ((method-lambda-list (flesh-out-lambda-list
+                               (generate-defrecgeneric-method-lambda-list
+                                lambda-list))))
+      (multiple-value-bind (required-params
+                            optional-params
+                            rest-param
+                            keyword-params
+                            allow-other-keys-p
+                            aux-params
+                            keys-p)
+          (alexandria:parse-ordinary-lambda-list method-lambda-list)
+        (declare (ignore allow-other-keys-p aux-params keys-p))
+        `(progn
+           (defgeneric ,name ,lambda-list
+             ,@options
+             (:method-combination superstandard))
+           (defmethod ,name :superaround ,method-lambda-list
+             ,(generate-function-body
+               `(call-next-method)
+               :test test
+               :custom-test custom-test
+               :custom-test-supplied-p custom-test-supplied-p
+               :name name
+               :required-params required-params
+               :optional-params optional-params
+               :rest-param rest-param
+               :keyword-params keyword-params)))))))
+
+
+
+;; TODO: Observe that DEFRECGENERIC doesn't contain itself to recording only
+;; the outermost function call. We can avoid this by using a dynamic, standard
+;; strategy. But if we do that, we might as well use the same strategy for
+;; DEFRECFUN and save the whole LABELS part.
+;; There's essentially three main approaches:
+;; 1) record all fn calls
+;; 2) record outer fn call only (i.e. first encountered recfun)
+;; 3) record outer fn call only per fn
+;; I'm not yet committed to either idea, but it's clearly a design question.
+;; Though, perhaps, I weakly prefer approach 3.
